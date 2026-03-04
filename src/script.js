ae044ee0220c17cbe67213f8117a6e0b491d5e31
Masagus Ahmad Ramadhan <agusyantosugiyanto@gmail.com>
2026-03-05 03:26:50 +0700

    adding login/register page

--- src/script.js
+++ src/script.js
@@ -10,3 +10,25 @@ function typeWriter() {
   }
 }
 document.addEventListener("DOMContentLoaded", typeWriter);
+
+function openLogin() {
+  location.href = "account.html"
+}
+
+let swipeState = false;
+const swiperCheck = document.getElementById("swiperCheck");
+function toggleSwipe() {
+  swipeState = !swipeState;
+  const title = document.querySelector(".sectTitle");
+  const info = document.querySelector(".sectInfo");
+  const button = document.querySelector(".sectButton");
+  if (swipeState) {
+    title.innerHTML = "Hello, Friend!";
+    info.innerHTML = "Enter your personal details and start journey with us";
+    button.innerHTML = "Sign Up";
+  } else {
+    title.innerHTML = "Welcome Back!";
+    info.innerHTML = "To keep connected with us please login with your personal info";
+    button.innerHTML = "Login";
+  }
+}
